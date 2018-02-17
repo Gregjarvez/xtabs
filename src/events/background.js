c58a759ad013bd82db088ad1afe93b9659a17a67
Gregory Assasie <gregjarvez@gmail.com>
2018-02-17 13:16:49 +0000

    removed logger middleware

--- src/events/background.js
+++ src/events/background.js
@@ -3,14 +3,6 @@ import { wrapStore } from 'react-chrome-redux';
 import rootReducer from './reducers';
 import IOEvents from '../scripts/events';
 
-const logger = store => next => (action) => {
-  console.group('start');
-  console.log(store.getState());
-  console.log(action);
-  console.groupEnd('start');
-  return next(action);
-};
-
 const store = createStore(rootReducer);
 
 // eslint-disable-next-line
