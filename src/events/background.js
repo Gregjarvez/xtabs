15a7a476b20a493714d411a08b3d5f10f4a2926b
Gregory Assasie <gregjarvez@gmail.com>
2018-04-05 23:44:26 +0100

    moved context menu initialisation to backgrounds

--- src/events/background.js
+++ src/events/background.js
@@ -2,11 +2,17 @@ import { createStore } from 'redux';
 import { wrapStore } from 'react-chrome-redux';
 import rootReducer from './reducers';
 import IOEvents from '../scripts/events';
+import ContextMenu from '../scripts/contextMenu';
 
 const store = createStore(rootReducer);
 
 // eslint-disable-next-line
-new IOEvents(store);
+const app = new IOEvents(store);
+const contextMenu = Object.create(ContextMenu);
+contextMenu
+  .removePreviousMenu()
+  .installMenu()
+  .initialActionListener(app);
 
 wrapStore(store, {
   portName: 'stackTabs'
